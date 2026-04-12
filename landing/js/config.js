d231ccbe80a54135e5edc194a11ee200db1276c9
fahrezyman <ilhamfahrezyxz@gmail.com>
2026-04-12 10:23:05 +0700

    fix: landing page DRY refactor, open CORS for public services endpoint

---
+++ landing/js/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  API_BASE_URL: "https://floyd-research-commissioners-insights.trycloudflare.com/api",
+  WHATSAPP_NUMBER: "6285188668489",
+  WHATSAPP_NUMBER_ALT: "6285168624911",
+  INSTAGRAM: "https://instagram.com/momcha.babycare",
+  TIKTOK: "https://tiktok.com/@momcha.babycare",
+};
