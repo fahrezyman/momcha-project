c079dfec687a11652626d7703adedfc98d1eb29a
fahrezyman <ilhamfahrezyxz@gmail.com>
2026-04-27 06:08:57 +0700

    sys: changing API calls for landing page services

--- landing/js/config.js
+++ landing/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_BASE_URL: "https://floyd-research-commissioners-insights.trycloudflare.com/api",
+  API_BASE_URL: "http://localhost:4000/api",
   WHATSAPP_NUMBER: "6285188668489",
   WHATSAPP_NUMBER_ALT: "6285168624911",
   INSTAGRAM: "https://instagram.com/momcha.babycare",
