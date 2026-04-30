8d659f0e2fbf0752fa5a145db34c51a0b735ca28
fahrezyman <ilhamfahrezyxz@gmail.com>
2026-04-30 20:02:28 +0700

    fix: update landing page API URL to production

--- landing/js/config.js
+++ landing/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_BASE_URL: "http://localhost:4000/api",
+  API_BASE_URL: "https://api.momcha.cloud/api",
   WHATSAPP_NUMBER: "6285188668489",
   WHATSAPP_NUMBER_ALT: "6285168624911",
   INSTAGRAM: "https://instagram.com/momcha.babycare",
